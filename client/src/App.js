179166f00f1634cd41958a67b5b4955d8305f0c1
raythurman2386 <raymondthurman5@gmail.com>
2019-12-15 21:19:48 -0500

    routes implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import ProjectList from "./components/ProjectList";
+import ProjectDetails from "./components/ProjectDetails";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <h1>App</h1>
+      <h1>
+        <Link to="/">App</Link>
+      </h1>
+      <Switch>
+        <Route exact path="/" component={ProjectList} />
+        <Route exact path="/details/:id" component={ProjectDetails} />
+      </Switch>
     </div>
   );
 }
