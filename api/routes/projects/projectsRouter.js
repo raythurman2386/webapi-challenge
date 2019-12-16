cff433094f0b61a6a30c2d8ec6a16aa4b2478e02
raythurman2386 <raymondthurman5@gmail.com>
2019-12-15 20:00:40 -0500

    projects routers working

--- api/routes/projects/projectsRouter.js
+++ api/routes/projects/projectsRouter.js
@@ -1,6 +1,7 @@
 const projectsRouter = require("express").Router();
 const Projects = require("../../../data/helpers/projectModel");
 
+// get all projects
 const getAllProjects = async (req, res) => {
   try {
     const projects = await Projects.get();
@@ -10,6 +11,7 @@ const getAllProjects = async (req, res) => {
   }
 };
 
+// Get project by id
 const getProjectById = async (req, res) => {
   try {
     const project = await Projects.get(req.params.id);
@@ -19,6 +21,41 @@ const getProjectById = async (req, res) => {
   }
 };
 
-projectsRouter.get("/", getAllProjects).get("/:id", getProjectById);
+// Add a project
+const addProject = async (req, res) => {
+  try {
+    const project = await Projects.insert(req.body);
+    res.status(201).json(project[0]);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// update a project
+const updateProject = async (req, res) => {
+  try {
+    const project = await Projects.update(req.params.id, req.body);
+    res.json(project[0]);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+//delete a project
+const deleteProject = async (req, res) => {
+  try {
+    const project = await Projects.remove(req.params.id);
+    res.json(project[0]);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+projectsRouter
+  .get("/", getAllProjects)
+  .get("/:id", getProjectById)
+  .post("/", addProject)
+  .put("/:id", updateProject)
+  .delete("/:id", deleteProject);
 
 module.exports = projectsRouter;
